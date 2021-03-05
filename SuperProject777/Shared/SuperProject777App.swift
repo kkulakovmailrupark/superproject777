103321323a883ed924b6cfe87b61eb4ea4d823e5
Konstantin Kulakov <mailrupark@kostyakulakov.ru>
2021-03-05 21:06:05 +0300

    Init xcode project

---
+++ SuperProject777/Shared/SuperProject777App.swift
@@ -0,0 +1,17 @@
+//
+//  SuperProject777App.swift
+//  Shared
+//
+//  Created by mailpark on 05.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SuperProject777App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
